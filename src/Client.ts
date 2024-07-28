d576db3c8a1d2b747a97aa9c4a9dbbe569d15806
berk ziya <berkziya1@gmail.com>
2024-07-28 22:15:17 +0300

    chore: Add ModelHandler and Client classes for browser automation

--- src/Client.ts
+++ src/Client.ts
@@ -9,7 +9,7 @@ import {
 import AsyncLock from 'async-lock';
 import fs from 'fs';
 import path from 'path';
-import { Player } from '../../entity/Player';
+import { Player } from './entity/Player';
 
 const iPhoneUserAgent =
   'Mozilla/5.0 (iPhone; CPU iPhone OS 17_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/127.0 Mobile/15E148 Safari/605.1.15';
